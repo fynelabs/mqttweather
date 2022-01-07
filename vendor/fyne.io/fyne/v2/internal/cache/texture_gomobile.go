7fe6a1b258c636b05dbc401498f8515f02650f70
Cedric BAIL <cedric@ddlm.me>
2022-01-07 14:59:35 -0700

    Update all dependencies and provide a vendor directory.

---
+++ vendor/fyne.io/fyne/v2/internal/cache/texture_gomobile.go
@@ -0,0 +1,16 @@
+//go:build android || ios || mobile
+// +build android ios mobile
+
+package cache
+
+import "fyne.io/fyne/v2/internal/driver/mobile/gl"
+
+// TextureType represents an uploaded GL texture
+type TextureType = gl.Texture
+
+var noTexture = gl.Texture{0}
+
+type textureInfo struct {
+	textureCacheBase
+	texture TextureType
+}
